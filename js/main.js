55df591632b4c2f9b154fd38a73a1db2ba49677d
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-11-07 23:31:44 -0600

    json -norm

--- js/main.js
+++ js/main.js
@@ -1,24 +1,26 @@
-const norma = documet.querySelector('#normas');
+const tabla = document.querySelector('#lista-usuarios tbody');
 
-function cargarNormas() {
-
+function cargarUsuarios() {
     fetch('normas.json')
-    .then(respuesta => respuesta.json())
-    .then(normas => {
-        normas.forEach(norma => {
-            const card =document.createElement('card');
-            card.classList = 'card';
-            card.innerHTML += `
-            <div class="container">
-            <div>${norma.id} </div>
-            <div>${norma.nombre} </div>
-            </div>
-            `;
-            norma.appendChild(card);
-        });
-    })
-    .catch(error => console.log('Hubo un error : ' + error.message))
+        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                 const card = document.createElement('card');
+                 card.classList = 'card';
+                card.innerHTML += `
+                                <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="width:100%">
+                                 <div class="container">
+                                     <h4><b>${usuario.id}</b></h4>
+                                        <p>${usuario.nombre}</p>
+                                     </div>
+                            
+                             `;
 
+                 tabla.appendChild(card);
+
+            });
+        }) // Aquí mostramos dicha información
+        .catch(error => console.log('Hubo un error : ' + error.message))
 }
 
-cargarNormas();
+cargarUsuarios();
